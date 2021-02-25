9e6345a9359f327d10b069b5e5b74c57e9dbf862
sascha7777 <43936809+sascha7777@users.noreply.github.com>
2021-02-25 22:31:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def index():
-    return "Praxisprojekt 2021 TEST"
+    return "Praxisprojekt 2021 TEST TEST"
 
 
 if __name__ == '__main__':
