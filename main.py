fae678917a26c0b3d5cd2289737a4b17bde2aad6
LukeWIN <luke.strauer@fs-students.de>
2021-02-25 21:47:12 +0100

    Hello Praxisprojekt

---
+++ main.py
@@ -0,0 +1,13 @@
+## external imports
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def index():
+    return "Hello Praxisprojekt"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
