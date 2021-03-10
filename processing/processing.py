578c40ed27577b79e4391749779d756ad9b06d1e
sascha7777 <saschaberger@hotmail.de>
2021-03-10 11:48:16 +0100

    95: Addition-Funktion in processing.py

---
+++ processing/processing.py
@@ -0,0 +1,15 @@
+
+
+def addition(summand1: float, summand2: float) -> float:
+    """
+    Sums up two numbers and returns the result as dictionary
+
+    :param summand1: The first summand to add
+    :type summand1: float
+    :param subtrahend: The second summand to add
+    :type summand2: float
+    :return: A dictionary with the key "sum" and the sum of both summands as value
+    """
+
+    return {"sum": summand1+summand2}
+
