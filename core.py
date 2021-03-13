240b58ff909ff577298f08c000df2dc8edc86cd3
sascha7777 <saschaberger@hotmail.de>
2021-03-13 20:39:18 +0100

    97: Added check for data type

--- core.py
+++ core.py
@@ -5,21 +5,26 @@ import processing.typeconversion as tc
 
 class Core():
 
-    def addition(self, json_data: str) -> str:
+    def addition(self, data: str) -> str:
 
         """
         Receives a json string, converts it to a dictionary and passes it to the processes.addition function.
         The returned sum gets converted to a dictionary which is returned 
 
-        :param json_data: string in json format
-        :type json_data: str
+        :param data: string in json format
+        :type data: str
 
         :return: A string in json format with the key "sum" and the sum of two numbers as value
         """
-
-        dict_data = tc.json_to_dict(json_data)
+        
+        if(type(data) != dict):
+            dict_data = tc.json_to_dict(data)
+        else:
+            dict_data = data
+        
         sum = proc.addition(dict_data["number1"], dict_data["number2"])
         json_return = tc.dict_to_json(sum)
 
         return json_return
+
 
