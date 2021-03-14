50d2f79943928bf6d192debde57b069cc818a781
lucabloe <luca.bloesel@fs-students.de>
2021-03-14 10:56:23 +0100

    92: added comments; removed debugging code; fixed code

--- frontend/static/js/js-handler.js
+++ frontend/static/js/js-handler.js
@@ -1,31 +1,33 @@
 /**
- * Sends call to backend and sets output-field to sum.
+ * Sends call to backend addition endpoint and shows sum in output-field.
  * 
  * 
  */
 
 function calc() {
+    // Get values from Input-Fields
     let input1 = document.getElementById("input-1").value;
     let input2 = document.getElementById("input-2").value;
-    
+
+    // Create new HTTP-Request to addition-endpoint
     let xhttp = new XMLHttpRequest();
-        xhttp.open("POST", "https://praxisprojekt-2021.ew.r.appspot.com/addition", true);
-        xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.open("POST", "https://praxisprojekt-2021.ew.r.appspot.com/addition", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
 
-        xhttp.onreadystatechange = function() {
-    if (this.readyState === XMLHttpRequest.DONE && (this.status === 200 || this.status === 201)) {
-        console.log(xhttp.responseText);
-        let response = xhttp.responseText;
-        // Prozessieren der Response
-        let json = JSON.parse(this.responseText);
-        let sum = json.sum;
-        document.getElementById("output").value = sum;
+    // Handle the Response from the HTTP-Request
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === XMLHttpRequest.DONE && (this.status === 200 || this.status === 201)) {
+            let response = xhttp.responseText;
+            // Process the Response + Show sum in Output-Feld
+            let json = JSON.parse(this.responseText);
+            let sum = json.sum;
+            document.getElementById("output").value = sum;
 
+        }
     }
-    let params = `{"number1": "${input-1}", "number2": "${input-2}"}`
+    // embed parameters for Request in a JSON-String
+    let params = `{"number1": "${input - 1}", "number2": "${input - 2}"}`;
 
-    // let params = JSON.stringify(param);
-        xhttp.send(params);
-
-    }
+    // send the HTTP-Request
+    xhttp.send(params);
 }
