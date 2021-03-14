91fdd39766589846cd98cc2a6a9e4a324d31a14d
lucabloe <luca.bloesel@fs-students.de>
2021-03-14 11:27:26 +0100

    modularized code

--- frontend/static/js/js-handler.js
+++ frontend/static/js/js-handler.js
@@ -4,11 +4,8 @@
  * 
  */
 
-function calc() {
-    // Get values from Input-Fields
-    let input1 = document.getElementById("input-1").value;
-    let input2 = document.getElementById("input-2").value;
-
+function calc(number1, number2) {
+    
     // Create new HTTP-Request to addition-endpoint
     let xhttp = new XMLHttpRequest();
     xhttp.open("POST", "https://praxisprojekt-2021.ew.r.appspot.com/addition", true);
@@ -16,8 +13,7 @@ function calc() {
 
     // Handle the Response from the HTTP-Request
     xhttp.onreadystatechange = function () {
-        if (this.readyState === XMLHttpRequest.DONE && (this.status === 200 || this.status === 201)) {
-            let response = xhttp.responseText;
+        if (this.readyState === XMLHttpRequest.DONE && (this.status >= 200 && this.status < 300)) {
             // Process the Response + Show sum in Output-Feld
             let json = JSON.parse(this.responseText);
             let sum = json.sum;
@@ -26,8 +22,7 @@ function calc() {
         }
     }
     // embed parameters for Request in a JSON-String
-    let params = `{"number1": "${input - 1}", "number2": "${input - 2}"}`;
-
+    let params  = JSON.stringify({number1: number1, number2: number2})
     // send the HTTP-Request
     xhttp.send(params);
 }
