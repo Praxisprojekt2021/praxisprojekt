9201bcabd5d1222297a9f85f7e37d05f1b1dfc2c
lucabloe <luca.bloesel@fs-students.de>
2021-03-16 17:50:52 +0100

    removed static content from html-file; moved de.json file; added comments

--- frontend/static/js/translator.js
+++ frontend/static/js/translator.js
@@ -1,15 +1,17 @@
 import i18next from 'https://deno.land/x/i18next/index.js';
-// import Backend from 'https://deno.land/x/i18next_fs_backend/index.js';
 import BackendAdapter from 'https://cdn.jsdelivr.net/gh/i18next/i18next-multiload-backend-adapter/src/index.js'
 import Fetch from 'https://cdn.jsdelivr.net/gh/dotcore64/i18next-fetch-backend/src/index.js'
 
+/**
+ * Fetches de.json from backend and then translates all HTML-elements which have a data-i18n attribute
+ */
 i18next
 .use(BackendAdapter)
 .init({
 backend: {
     backend: Fetch,
     backendOption:{
-      loadPath: '/static/i18n/de.json',
+      loadPath: '/static/content/i18n/de.json',
       allowMultiLoading:  true,
       multiSeparator: '+',
     }
@@ -18,9 +20,9 @@ lng: 'de',
 fallbackLng: 'de',
 preload: ['de'],
 }).then(function(t) {
-    // after initialization is done, translate all HTML-Elements which have a data-18in attribute
-    // attribute must have the same value as the corresponding key in the de.json-file
-    for (let i = 0; i < document.querySelectorAll('[data-i18n]').length; i++) { 
+    // after initialization is done, translate all HTML-Elements which have a data-i18n attribute
+    // data-i18n attribute must have the same value as the corresponding key in the de.json-file
+    for (let i = 0; i < document.querySelectorAll('[data-i18n]').length; i++) {
         document.querySelectorAll('[data-i18n]')[i].innerHTML = i18next.t(document.querySelectorAll('[data-i18n]')[i].getAttribute("data-i18n"));
     }
 });
