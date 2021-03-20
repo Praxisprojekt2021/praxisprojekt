d15463c98e1dce9b0fe60c0db1faf70183b0474b
sascha7777 <saschaberger@hotmail.de>
2021-03-20 19:18:22 +0100

    150: Added component_view_route and

--- api/endpoints.py
+++ api/endpoints.py
@@ -4,7 +4,8 @@ import core
 from api.error_handler import error_handler
 
 
-app = Flask(__name__, static_url_path='', template_folder='../frontend/templates', static_folder='../frontend/static')
+app = Flask(__name__, static_url_path='',
+            template_folder='../frontend/templates', static_folder='../frontend/static')
 
 
 @app.route('/', methods=["GET"])
@@ -27,5 +28,20 @@ def addition_route():
 
     if request.is_json:
         return core.core_addition(request.json), 200
+    else:
+        return error_handler(400, "No JSON body was transferred")
+
+
+@app.route('/component/view', methods=["POST"])
+def component_view_route():
+    """
+    API endpoint to use the function from the Core-Module to view components
+    """
+
+    if request.is_json:
+        try:
+            return core.component_view(request.json), 200
+        except:
+            return "Internal Error", 500
     else:
         return error_handler(400, "No JSON body was transferred")
--- core.py
+++ core.py
@@ -1,5 +1,6 @@
 import processing.calculations as calculations
 import processing.typeconversion as typeconversion
+import database.handler.component_handler as component_handler
 
 
 def core_addition(input_object: str) -> str:
@@ -11,9 +12,27 @@ def core_addition(input_object: str) -> str:
     :type input_object: str
     :return: A JSON object with the key "sum" and the sum of two numbers as value
     """
-    
+
     data_dict = typeconversion.json_to_dict(input_object)
     result = calculations.addition(data_dict["number1"], data_dict["number2"])
+    output_object = typeconversion.dict_to_json(result)
+
+    return output_object
+
+
+def component_view(input_object: str) -> str:
+    """
+    Receives a JSON object in the form defined under JSON_objects_defitnions.py for getting/viewing a component.
+    It returns another JSON object, sturctured as described in docu/JSON_objects_definitions.py
+    which is retreived from the component_handler.
+
+    :param input_object: JSON object containing the component id
+    :type input_object: str
+    :return: Returns a JSON object, sturctured as described in docu/JSON_objects_definitions.py
+    """
+
+    data_dict = typeconversion.json_to_dict(input_object)
+    result = component_handler.get_component(data_dict)
     output_object = typeconversion.dict_to_json(result)
 
     return output_object
