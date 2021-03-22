01fc922d655de9119f698159a1969a0bb1f397bb
Timo Buecher <mail@timo-buecher.de>
2021-03-22 21:43:02 +0100

    183: Solved TODOS in component_handler.py

--- database/handler/component_handler.py
+++ database/handler/component_handler.py
@@ -1,5 +1,6 @@
+import neomodel
 from neomodel import config, StructuredNode, StringProperty, UniqueIdProperty, \
-    RelationshipTo, StructuredRel, FloatProperty
+    RelationshipTo, StructuredRel, FloatProperty, DateTimeProperty
 from database.handler.metric_handler import Metric
 import database.handler.metric_handler as mh
 from datetime import datetime
@@ -46,8 +47,8 @@ class Component(StructuredNode):
     name = StringProperty()
     category = StringProperty()
     description = StringProperty()
-    creation_timestamp = StringProperty()  # TODO: Use String or datetime?
-    last_timestamp = StringProperty()  # TODO: Use String or datetime?
+    creation_timestamp = StringProperty()  # evtl. float
+    last_timestamp = StringProperty()  # evtl. float
 
     hasMetric = RelationshipTo(Metric, "has", model=Relationship)
 
@@ -75,7 +76,7 @@ def get_component(uid_dict: dict) -> dict:
     Function to retrieve a single component
 
     :param uid_dict: Component uid
-    :type uid_dict: str
+    :type uid_dict: dict
     :return: Component dict
     """
 
@@ -102,15 +103,21 @@ def add_component(input_dict: dict) -> dict:
     :type input_dict: dict
     :return: Component dict
     """
+    successful = True
 
     output = Component(name=input_dict["name"], category=input_dict["category"],
                        creation_timestamp=str(datetime.now()),
-                       last_timestamp=str(datetime.now()), description=input_dict["description"]).save()
+                       last_timestamp=str(datetime.now()), description=input_dict["description"])
+    if not output.save():
+        successful = False
 
     for metric in input_dict["metrics"]:
-        output.hasMetric.connect(mh.get_metric(metric), {"value": input_dict["metrics"][metric]})
+        try:
+            output.hasMetric.connect(mh.get_metric(metric), {"value": input_dict["metrics"][metric]})
+        except neomodel.core.DoesNotExist:
+            successful = False
 
-    return {"success": True}    # TODO: ist es möglich den success aus der Datenbank zu bekommen und nicht pauschaul zurückzuliefern?
+    return {"success": successful}
 
 
 def update_component(input_dict: dict) -> dict:
@@ -129,6 +136,9 @@ def update_component(input_dict: dict) -> dict:
     component.category = input_dict["category"]
     component.last_timestamp = str(datetime.now())
 
+    successful = True
+    if not component.save():
+        successful = False
     component_dict = get_component({"uid": uid})
 
     metrics_dict = component_dict["metrics"]
@@ -141,9 +151,10 @@ def update_component(input_dict: dict) -> dict:
         metric_object = mh.get_metric(metric)
         rel = component.hasMetric.relationship(metric_object)
         rel.value = new_metrics[metric]
-        rel.save()
+        if not rel.save():
+            successful = False
 
-    return {"success": True}    # TODO: ist es möglich den success aus der Datenbank zu bekommen und nicht pauschaul zurückzuliefern?
+    return {"success": successful}
 
 
 def delete_component(uid_dict: dict) -> dict:
@@ -151,7 +162,7 @@ def delete_component(uid_dict: dict) -> dict:
     Function to delete a single component
 
     :param uid_dict: Identifier
-    :type uid_dict: str
+    :type uid_dict: dict
     :return: dict
     """
     if Component.nodes.get(uid=uid_dict["uid"]).delete():
