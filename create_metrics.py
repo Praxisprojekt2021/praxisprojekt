b44215ca532cbdc00fb58e479a750ab517477b1e
Timo Buecher <mail@timo-buecher.de>
2021-03-24 13:54:45 +0100

    183: Added dynamic metrics creation out of the frontend json definition

--- create_metrics.py
+++ create_metrics.py
@@ -1,2 +1,3 @@
-from database.handler.metric_handler import create_from_csv
-create_from_csv("database/metrics.csv")
+from database.handler.metric_handler import create_from_frontend_json
+
+create_from_frontend_json('docu/mapping_metrics_definition.json')
--- database/handler/metric_handler.py
+++ database/handler/metric_handler.py
@@ -1,8 +1,10 @@
 import csv
+import json
 
 from neomodel import config, StructuredNode, StringProperty, UniqueIdProperty
 
 from database.config import *
+from processing import json_to_dict
 
 config.DATABASE_URL = 'bolt://{}:{}@{}:{}'.format(NEO4J_USER, NEO4J_PASSWORD, NEO4J_IP, NEO4J_PORT)
 
@@ -15,13 +17,12 @@ class Metric(StructuredNode):
     ----------
     name : str
         name of the metric
-    description : str
-        description of the metric
+    uid : str
+        unique id of the metric
    """
 
     uid = UniqueIdProperty()
     name = StringProperty()
-    description = StringProperty()
 
 
 def create_from_csv(path: str):
@@ -36,8 +37,24 @@ def create_from_csv(path: str):
         csv_reader_object = csv.reader(csv_file)
         for row in csv_reader_object:
             print(row)
-            Metric.create({'name': row[0], 'description': row[1]})
+            Metric.create({'name': row[0]})
         print(Metric.nodes.all())
+
+
+def create_from_frontend_json(path: str):
+    """
+    Function to create metrics out of the frontend definition file
+    """
+    with open(path) as json_file:
+        data = json.load(json_file)
+        data_dict = json_to_dict(data)
+        features = data_dict["features"]
+
+        for key in features:
+            feature = features[key]
+            metrics = feature["metrics"]
+            for metric in metrics:
+                Metric.create({'name': metric})
 
 
 def get_metric(input_name: str) -> Metric:
