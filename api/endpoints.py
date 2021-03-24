cd7c1cd8d49eda1c6cc1824e57a642ae69718d3f
Sofie273 <sofie.sharaf@fs-students.de>
2021-03-24 22:49:38 +0100

    added process hdler, added endpoints, ep clean up

--- api/endpoints.py
+++ api/endpoints.py
@@ -8,40 +8,38 @@ app = Flask(__name__, static_url_path='',
 
 
 @app.route('/', methods=["GET"])
-def index():
+def index_route():
     """
     API endpoint to display main page view
-
+    
+    :receives: None
     :return: the rendered html main page
     """
     return render_template("index.html")
 
 
+"""
+    API endpoints for component operations
+"""
+
+
 @app.route('/component', methods=["GET"])
-def component():
+def component_route():
     """
     API endpoint to display component page view
 
+    :receives: None
     :return: the rendered html component page
     """
     return render_template("component.html")
 
 
-@app.route('/process', methods=["GET"])
-def process():
-    """
-    API endpoint to display component page view
-
-    :return: the rendered html process page
-    """
-    return core.error_handler(500, "Noch nicht umgesetzt")
-
-
 @app.route("/component/overview", methods=["GET"])
-def get_component_overview():
+def component_overview_route():
     """
     API Endpoint returning all components for the Index site
-    :receives None
+
+    :receives: None
     :return: a JSON object containing a list of components
     """
 
@@ -49,31 +47,13 @@ def get_component_overview():
 
     return components
 
-
-@app.route("/component/delete", methods=["POST"])
-def do_component_delete():
-    """
-    API Endpoint to delete a specific component
-
-    :return: a JSON object with the success of the deletion
-    """
-    x = request.data
-    if request.is_json:
-        try:
-            result = core.delete_component(request.json)
-
-            return result, 200
-
-        except:
-            return "Internal Error", 500
-    else:
-        return "No JSON body was transferred", 400
-
-
 @app.route('/component/view', methods=["POST"])
 def component_view_route():
     """
     API endpoint to use the function from the Core-Module to view components
+    
+    :receives: a JSON object with component details as specified in docu/JSON_objects_definitions.py
+    :return: a JSON object containing the component
     """
 
     if request.is_json:
@@ -89,9 +69,152 @@ def component_view_route():
 def component_create_edit_route():
     """
     Use function from the Core-Module to create or edit components (Original function is located in component_handler.py
+    
+    :receives: a JSON object with component details as specified in docu/JSON_objects_definitions.py
+    :return: a JSON object containing the success state, which is True or False
     """
 
     if request.is_json:
         return core.create_edit_component(request.json), 200
+    else:
+        return core.error_handler(400, "No JSON body was transferred")
+
+
+@app.route("/component/delete", methods=["POST"])
+def component_delete_route():
+    """
+    API Endpoint to delete a specific component
+
+    :receives: a JSON object with component details as specified in docu/JSON_objects_definitions.py
+    :return: a JSON object with the success of the deletion
+    """
+
+    if request.is_json:
+        try:
+            return core.delete_component(request.json), 200
+
+        except:
+            return "Internal Error", 500
+    else:
+        return "No JSON body was transferred", 400
+
+
+"""
+    API endpoints for process operations 
+"""
+
+
+@app.route('/process', methods=["GET"])
+def process_route():
+    """
+    API endpoint to display process page view
+    
+    :receives: None
+    :return: the rendered html process page
+    """
+    return core.error_handler(500, "Noch nicht umgesetzt")
+
+
+@app.route('/process/overview', methods=["GET"])
+def process_overview_route():
+    """
+    API Endpoint returning all processes for the Index site
+
+    :receives: None
+    :return: a JSON object containing a list of processes
+    """
+
+    processes = core.get_process_list()
+
+    return processes
+
+@app.route('/process/delete', methods=["POST"])
+def process_delete_route():
+    """
+    API Endpoint to delete a specific process
+    
+    :receives: a JSON object with process details as specified in docu/JSON_objects_definitions.py
+    :return: a JSON object with the success of the deletion
+    """
+
+    if request.is_json:
+        try:
+            return core.delete_process(request.json), 200
+
+        except:
+            return "Internal Error", 500
+    else:
+        return "No JSON body was transferred", 400
+
+
+@app.route('/process/edit/createstep', methods=["POST"])
+def process_edit_createstep_route():
+    """
+    API Endpoint to add a step to specific process
+    
+    :receives: a JSON object with process details as specified in docu/JSON_objects_definitions.py
+    :return: a JSON object with process details as specified in docu/JSON_objects_definitions.py
+    """
+    if request.is_json:
+        return core.update_process_reference(request.json), 200
+    else:
+        return core.error_handler(400, "No JSON body was transferred")
+
+
+@app.route('/process/edit/editstep', methods=["POST"])
+def process_edit_editstep_route():
+    """
+    API Endpoint to edit a step from specific process
+    
+    :receives: a JSON object with process details as specified in docu/JSON_objects_definitions.py
+    :return: a JSON object with process details as specified in docu/JSON_objects_definitions.py
+    """
+    if request.is_json:
+        return core.add_process_reference(request.json), 200
+    else:
+        return core.error_handler(400, "No JSON body was transferred")
+
+
+@app.route('/process/edit/deletestep', methods=["POST"])
+def process_edit_deletestep_route():
+    """
+    API Endpoint to delete a step from specific process
+    
+    :receives: a JSON object with process details as specified in docu/JSON_objects_definitions.py
+    :return: a JSON object with process details as specified in docu/JSON_objects_definitions.py
+    """
+    if request.is_json:
+        return core.delete_process_reference(request.json), 200
+    else:
+        return core.error_handler(400, "No JSON body was transferred")
+
+
+@app.route('/process/create_edit', methods=["POST"])
+def process_create_edit_route():
+    """
+    API Endpoint to create or edit a process
+    
+    :receives: a JSON object with process details as specified in docu/JSON_objects_definitions.py
+    :return: a JSON object with process details as specified in docu/JSON_objects_definitions.py
+    """
+    if request.is_json:
+        return core.create_edit_process(request.json), 200
+    else:
+        return core.error_handler(400, "No JSON body was transferred")
+
+
+@app.route('/process/view', methods=["GET"])
+def process_view_route():
+    """
+    API endpoint to use the function from the Core-Module to view processes
+    
+    :receives: a JSON object with process details as specified in docu/JSON_objects_definitions.py
+    :return: a JSON object containing the process
+    """
+    if request.is_json:
+        try:
+            return core.get_process(request.json), 200
+        except:
+            return "Internal Error", 500
     else:
         return core.error_handler(400, "No JSON body was transferred")
--- core/core.py
+++ core/core.py
@@ -1,17 +1,37 @@
 from typing import Dict, Any, Union
 
-import database
+import database.handler.process_handler as process_handler
+import database.handler.component_handler as component_handler
+
 import processing
 
 
 def get_component_list() -> str:
-    """Calls the get_compontent_list method and converts the output to JSON
-
-    Returns:
-        str: A JSON formatted compontent list
     """
-    component_list_dict = database.get_component_list()
+    Calls the get_compontent_list method and converts the output to JSON
+
+    :return: A JSON formatted compontent list
+    """
+    component_list_dict = component_handler.get_component_list()
     output_json = processing.dict_to_json(component_list_dict)
+
+    return output_json
+
+
+def get_component(input_object: str) -> str:
+    """
+    Receives a JSON object in the form defined under JSON_objects_defitnions.py for getting/viewing a component.
+    It returns another JSON object, sturctured as described in docu/JSON_objects_definitions.py
+    which is retreived from the component_handler.
+
+    :param input_object: JSON object containing the component uid
+    :type input_object: str
+    :return: Returns a JSON object, structured as described in docu/JSON_objects_definitions.py
+    """
+
+    data_dict = processing.json_to_dict(input_object)
+    component_dict = component_handler.get_component(data_dict)
+    output_json = processing.dict_to_json(component_dict)
 
     return output_json
 
@@ -23,51 +43,134 @@ def create_edit_component(input_object: Union[Dict[str, Any], str]) -> str:
     
     :param input_object: JSON object containing all component attributes (special information to differentiate edit or create is contained in the UID, which is either -1 or the original UID
     :type input_object: str
-    :return A JSON object containing the success state, which is True or False
+    :return: A JSON object containing the success state, which is True or False
     """
 
     data_dict = processing.json_to_dict(input_object)
 
     if data_dict["uid"] == "-1":
-        result_dict = database.add_component(data_dict)
+        result_dict = component_handler.add_component(data_dict)
         return processing.dict_to_json(result_dict)
     else:
-        result_dict = database.update_component(data_dict)
+        result_dict = component_handler.update_component(data_dict)
         return processing.dict_to_json(result_dict)
-
-
-def get_component(input_object: str) -> str:
-    """
-    Receives a JSON object in the form defined under JSON_objects_defitnions.py for getting/viewing a component.
-    It returns another JSON object, sturctured as described in docu/JSON_objects_definitions.py
-    which is retreived from the component_handler.
-
-    :param input_object: JSON object containing the component uid
-    :type input_object: str
-    :return: Returns a JSON object, sturctured as described in docu/JSON_objects_definitions.py
-    """
-
-    data_dict = processing.json_to_dict(input_object)
-    component_dict = database.get_component(data_dict)
-    output_json = processing.dict_to_json(component_dict)
-
-    return output_json
 
 
 def delete_component(input_object: str) -> str:
-    """Calls the delete_component method and returns whether successful or not in JSON Format
+    """
+    Calls the delete_component method and returns whether successful or not in JSON Format
 
-    Args:
-        data (str): String in JSON Format
-
-    Returns:
-        (str): String in JSON Format
-
-    :param input_object:
+    :param input_object: JSON object containing the component uid
+    :type input_object: str
+    :return: String in JSON Format
     """
 
     data_dict = processing.json_to_dict(input_object)
-    result_dict = database.delete_component(data_dict)
+    result_dict = component_handler.delete_component(data_dict)
     output_json = processing.dict_to_json(result_dict)
 
     return output_json
+
+
+def get_process_list() -> str:
+    """
+    Calls the get_process_list method and converts the output to JSON
+
+    :return: A JSON formatted compontent list
+    """
+    process_list_dict = process_handler.get_process_list()
+    output_json = processing.dict_to_json(process_list_dict)
+
+    return output_json
+
+
+def get_process(input_object: str) -> str:
+    """
+    Receives a JSON object in the form defined under JSON_objects_defitnions.py for getting/viewing a process.
+    It returns another JSON object, sturctured as described in docu/JSON_objects_definitions.py
+    which is retreived from the process_handler.
+
+    :param input_object: JSON object containing the process uid
+    :type input_object: str
+    :return: Returns a JSON object, structured as described in docu/JSON_objects_definitions.py
+    """
+    data_dict = processing.json_to_dict(input_object)
+
+    #TO DO Risk Calculation
+    
+    return XXX
+
+
+def create_edit_process(input_object: Union[Dict[str, Any], str]) -> str:
+    """
+    Receives a JSON object in the form defined under JSON_objects_defitnions.py for either editing a process or creating a new process
+    The answer is also a JSON object, only containing the success state, which is True or False
+    
+    :param input_object: JSON object containing all process attributes (special information to differentiate edit or create is contained in the UID, which is either -1 or the original UID
+    :type input_object: str
+    :return: A JSON object containing the success state, which is True or False
+    """
+
+    data_dict = processing.json_to_dict(input_object)
+
+    #TO DO create if 
+
+    return XXX
+
+
+def delete_process(input_object: str) -> str: 
+    """
+    Calls the delete_process method and returns whether successful or not in JSON Format
+
+    :param input_object: JSON object containing the process uid
+    :type input_object: str
+    :return: String in JSON Format
+    """
+
+    data_dict = processing.json_to_dict(input_object)
+    result_dict = process_handler.delete_process(data_dict)
+    output_json = processing.dict_to_json(result_dict)
+
+    return output_json
+
+
+def add_process_reference(input_object: str]) -> str:
+    """
+    Calls the add_process_reference method and returns whether successful or not in JSON Format
+
+    :param input_object: JSON object containing the process uid + component uid + weight 
+    :type input_object: str
+    :return: String in JSON Format
+    """
+
+    data_dict = processing.json_to_dict(input_object)
+
+    #TO DO Call view process funktion and return process JSON to frontend
+
+
+def update_process_reference(input_object: str]) -> str:
+    """
+    Calls the update_process_reference method and returns whether successful or not in JSON Format
+
+    :param input_object: JSON object containing the process uid + old_weight + new_weight
+    :type input_object: str
+    :return: String in JSON Format
+    """
+
+    data_dict = processing.json_to_dict(input_object)
+
+    #TO DO Call view process funktion and return process JSON to frontend
+
+
+def delete_process_reference(input_object: str]) -> str:
+    """
+    Calls the delete_process_reference method and returns whether successful or not in JSON Format
+
+    :param input_object: JSON object containing the process uid + weight
+    :type input_object: str
+    :return: String in JSON Format
+    """
+    
+    data_dict = processing.json_to_dict(input_object)
+
+    #TO DO Call view process funktion and return process JSON to frontend
