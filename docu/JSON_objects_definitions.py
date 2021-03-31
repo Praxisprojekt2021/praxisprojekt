e2b1536e0fcec545d8369fb52ba3fa08940226ef
Jasmin Capka <44087726+NimsaJ150@users.noreply.github.com>
2021-03-31 18:44:36 +0200

    Update JSON_objects_definitions.py

--- docu/JSON_objects_definitions.py
+++ docu/JSON_objects_definitions.py
@@ -226,7 +226,7 @@ data = {
             "actual": {
                 "average": 30,
                 "max": 50,
-                "min": 20,,
+                "min": 20,
                 "standard_deviation": 5,
                 "total": 300,  # summe
             },
@@ -256,7 +256,7 @@ data = {
             "actual": {
                 "average": 0.65,
                 "max": 0.8,
-                "min": 0.5,,
+                "min": 0.5,
                 "standard_deviation": 5,
                 "total": 0.4,     # summe
             },
@@ -426,7 +426,7 @@ data = {
             "actual": {
                 "average": 30,
                 "max": 50,
-                "min": 20,,
+                "min": 20,
                 "standard_deviation": 5,
                 "total": 300,  # summe
             },
@@ -456,7 +456,7 @@ data = {
             "actual": {
                 "average": 0.65,
                 "max": 0.8,
-                "min": 0.5,,
+                "min": 0.5,
                 "standard_deviation": 5,
                 "total": 0.4,     # summe
             },
