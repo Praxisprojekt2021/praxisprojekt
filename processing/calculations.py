85223dba22dab9966018c60d2e4a84b61373ae78
Nico <nic4schmids@gmail.com>
2021-04-01 13:19:42 +0200

    211 : calculation of current process metrics

--- processing/calculations.py
+++ processing/calculations.py
@@ -1,4 +1,8 @@
-def start_calculate_risk(process_dict: dict, metrics_dict: dict) -> dict:
+from typing import Dict, Any
+from statistics import mean, stdev
+
+
+def start_calculate_risk(process_dict: Dict[str, Any], metrics_dict: Dict[str, Any]) -> Dict[str, Any]:
     """
     The entry function and handler for the risk calculations. The risk score is calculated and returned with all related
     values.
@@ -10,4 +14,33 @@ def start_calculate_risk(process_dict: dict, metrics_dict: dict) -> dict:
     :return: Dict[str: Any]
     """
 
-    print(metrics_dict)
+    current_val = calculate_current_values(process_dict)
+
+
+def calculate_current_values(process_dict):
+    """
+    Function that extracts the current metrics values from a process dict
+
+   :param process_dict: A dict containing all relevant data of a process to calculate the risk score
+   :type process_dict: Dict[str: Any]
+   :return: Dict[str: Any]
+   """
+
+    metrics_dict = {}
+    for components in (process_dict["process"]["components"]):
+        for key, value in components["metrics"].items():
+            if key not in metrics_dict:
+                metrics_dict[key] = [value]
+            else:
+                metrics_dict[key].append(value)
+
+    calculations = {}
+    for key, value in metrics_dict.items():
+        calculations[key] = {"sum": sum(value),
+                             "min": min(value),
+                             "max": max(value),
+                             "avg": int(mean(value)),
+                             "std_dev": int(stdev(value))
+                             }
+
+    return calculations
