b4736ca72c1d334efafac371f11997349f221ae5
jonathan112358 <jonathan112358@Mac.fritz.box>
2021-04-02 17:09:19 +0200

    223: Änderung der Metriken Datei und Anpassung

--- database/handler/metric_handler.py
+++ database/handler/metric_handler.py
@@ -1,4 +1,3 @@
-import csv
 import json
 
 from neomodel import config, StructuredNode, StringProperty, UniqueIdProperty
@@ -15,10 +14,12 @@ class Metric(StructuredNode):
 
     Attributes
     ----------
-    name : str
-        name of the metric
     uid : str
         unique id of the metric
+    name : str
+        name of the metric
+    fulfilled_if: str
+        string containing whether the metric is fulfilled with a result > or < the result
    """
 
     uid = UniqueIdProperty()
@@ -26,37 +27,22 @@ class Metric(StructuredNode):
     fulfilled_if = StringProperty()
 
 
-def create_from_csv(path: str):
-    """
-    Function to create metrics out of an csv file
-
-    :param path: Path to the csv file
-    :type path: int
-    """
-
-    with open(path) as csv_file:
-        csv_reader_object = csv.reader(csv_file)
-
-    for row in csv_reader_object:
-        print(row)
-        Metric.create({'name': row[0]})
-    print(Metric.nodes.all())
-
-
-def create_from_frontend_json(path: str):
+def create_from_frontend_json(path: str) -> dict:
     """
     Function to create metrics out of the frontend definition file
     """
+
     with open(path) as json_file:
         data = json.load(json_file)
 
     features = data["features"]
 
     for key in features:
-        feature = features[key]
-        metrics = feature["metrics"]
-        for metric in metrics:
-            Metric.create({'name': metric})
+        for metric in features[key]["metrics"]:
+            for fulfill in features[key]["metrics"][metric]["fulfilled_if"]:
+                Metric.create({'name': metric, "fulfilled_if": fulfill})
+
+    return success_handler()
 
 
 def get_metric(input_name: str) -> Metric:
@@ -65,34 +51,25 @@ def get_metric(input_name: str) -> Metric:
 
     :param input_name: Name of the metric
     :type input_name: str
+    :return: Metric
     """
+
     return Metric.nodes.get(name=input_name)
 
 
 def get_metrics_data() -> dict:
     """
     Function to get all metrics and their attributes
+
+    :return: dict
     """
 
     metrics = Metric.nodes.all()
-    metrics_data_dict = success_handler()
+    metrics_dict = success_handler()
+    metrics_dict["metrics"] = {}
 
     for metric in metrics:
         metric_dict = metric.__dict__
-        metric_name = metric_dict.pop('name')
-        metrics_data_dict[metric_name] = metric_dict
-
-        # TODO: delete the following two codelines
-        metrics_data_dict[metric_name]["fulfilled_if"] = ">"  # oder "<"
+        metrics_dict["metrics"][metric_dict.pop('name')] = metric_dict
 
     return metrics_data_dict
-
-
-def add_metric(input_dict: dict) -> Metric:
-    """
-    Function to create metric
-
-    :param input_dict: Metric dict
-    :type input_dict: dict
-    """
-    return Metric(name=input_dict["name"], description=input_dict["description"]).save()
