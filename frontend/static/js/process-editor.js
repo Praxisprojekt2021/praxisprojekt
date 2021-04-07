18a5e889ef3304a10e88f265793bbce8ca9e4e26
lucabloe <luca.bloesel@fs-students.de>
2021-04-07 18:47:52 +0200

    created process js file; added scripts to process html

---
+++ frontend/static/js/process-editor.js
@@ -0,0 +1,10 @@
+//Base url to distinguish between localhost and production environment
+const base_url = window.location.origin;
+// instantiate object of helper class
+const helper = new Helper();
+
+document.addEventListener("DOMContentLoaded", init(), false);
+
+function init() {
+
+}
