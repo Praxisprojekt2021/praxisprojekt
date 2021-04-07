b4af523edc108287c9758c5dd42140de4c9b51ec
lucabloe <luca.bloesel@fs-students.de>
2021-04-07 20:08:21 +0200

    added div with process score circle2

--- frontend/static/js/process-editor.js
+++ frontend/static/js/process-editor.js
@@ -3,8 +3,18 @@ const base_url = window.location.origin;
 // instantiate object of helper class
 const helper = new Helper();
 
-document.addEventListener("DOMContentLoaded", init(), false);
+// document.addEventListener("DOMContentLoaded", init(), false);
 
 function init() {
 
+    this.renderWholeProcessScoreCircle(12);
+}
+
+function renderWholeProcessScoreCircle(wholeProcessScore) {
+    if(wholeProcessScore < 75) {
+        document.getElementById("whole-process-score").setAttribute("background-color", "red");
+    } else {
+        document.getElementById("whole-process-score").setAttribute("background-color", "green");
+    }
+    document.getElementById("whole-process-score").innerHTML = `${wholeProcessScore}%`;
 }
