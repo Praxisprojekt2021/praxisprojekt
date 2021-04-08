fce67f407e5d8eadfbf805a2eb37f4bf2096b6fb
jonathan112358 <74042443+jonathan112358@users.noreply.github.com>
2021-04-08 12:58:06 +0200

    201&199: kleine Anpassung

--- create_metrics.py
+++ create_metrics.py
@@ -2,4 +2,4 @@ import database.handler.metric_handler as mh
 import database.handler.process_handler as ph
 import database.handler.component_handler as ch
 
-# print(ph.update_process(data))
+# print(mh.create_from_frontend_json('frontend/static/content/mapping_metrics_definition.json'))
