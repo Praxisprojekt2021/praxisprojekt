8f85903b57f39542606afd4e0ddc3d8bfbe8b586
jasmincapka <jasmin.capka@fs-students.de>
2021-04-11 04:42:35 +0200

    212: Zwischenstand Metriken Tabelle

--- frontend/static/js/process-editor.js
+++ frontend/static/js/process-editor.js
@@ -20,42 +20,6 @@ function init() {
 }
 
 /**
- * Render process ball for whole process.
- * @param wholeProcessScore
- */
-function renderWholeProcessScoreCircle(wholeProcessScore) {
-    let color;
-    wholeProcessScore = parseInt(wholeProcessScore);
-
-    if(wholeProcessScore < 80) {
-        color = "red";
-    } else if (wholeProcessScore < 90) {
-        color = "yellow"
-    } else {
-        color = "green";
-    }
-    document.getElementById("whole-process-score").setAttribute("style", `background-color:  ${color}`);
-    document.getElementById("whole-process-score").innerHTML = `${wholeProcessScore}%`;
-}
-
-/**
- * Render ball for each metric.
- *
- * @param fulfillment
- * @returns {string}
- */
-function renderCircle(fulfillment) {
-    let color;
-    if(fulfillment) {
-        color = "green";
-    } else {
-        color = "red";
-    }
-
-    return `<div class="small-circle" style="background-color: ${color}"></div>`;
-}
-
-/**
 * Get list of features.
  */
 async function getFeatures() {
@@ -103,8 +67,9 @@ function getProcess(features) {
 
         // Trigger function which gathers process data and processes it
         const post_data = `{
-        "uid": "${uid}"
-    }`
+            "uid": "${uid}"
+        }`
+
         const base_url = window.location.origin;
         let xhttp = new XMLHttpRequest();
         xhttp.open("POST", base_url + "/process/view", true);
@@ -115,7 +80,7 @@ function getProcess(features) {
             if (this.readyState === XMLHttpRequest.DONE && (this.status >= 200 && this.status < 300)) {
                 // Process response and show sum in output field
                 let processData = JSON.parse(this.responseText);
-                createMetricsSection(features, processData);
+                fillDataFields(features, processData);
             }
         }
         xhttp.send(post_data);
@@ -127,6 +92,200 @@ function getProcess(features) {
     }
 
 }
+
+
+
+/**
+ * This functions toggles the accordion
+ *
+ * @param {HTMLElement} element: HTML accordion to be either opened oder closed
+ */
+
+function fillDataFields(features, processData) {
+    if (processData['success']) {
+        // fill description column
+        fillDescriptionColumn(processData);
+        // create metric/feature toggle area
+        createMetricsSection(features, processData);
+        //
+    } else {
+        // Component has not been created/edited successfully
+        window.alert('Process could not be loaded.');
+    }
+
+}
+
+/**
+ * This functions toggles the accordion
+ *
+ * @param {json} processData
+ */
+
+function fillDescriptionColumn(processData) {
+
+    this.renderWholeProcessScoreCircle(processData['score']);
+
+    // Set uid and data fields
+    document.getElementById('process-name-textarea').value = processData['process']['name'];
+    document.getElementById('process-beschreibung-textarea').value = processData['process']['description'];
+
+}
+
+/**
+ * Render metrics section.
+ *
+ * @param {json} features
+ * @param {json} processData
+ */
+function createMetricsSection(features, processData) {
+    let featureCount=0;
+    Object.keys(features).forEach(function (key) {
+        featureCount++;
+        let feature = features[key];
+        let metrics = feature['metrics'];
+
+        let div = document.createElement('div');
+        div.id = key;
+        div.className = 'feature-section';
+
+        let innerHTML = '';
+        innerHTML += '<div data-hover="" data-delay="0" class="accordion-item">';
+        innerHTML += '<div class="accordion-toggle" onclick="toggleSection(this)">';
+        innerHTML += '<div class="accordion-icon"></div>';
+        innerHTML += '<div class="features-label">' + feature['name'] + '</div>';
+        innerHTML += '</div>';
+        innerHTML += '<nav class="dropdown-list">';
+        innerHTML += '<div class="features-columns">';
+
+        // Table Headers
+        innerHTML += `
+        <label>Feature ${featureCount}: ${feature['name']}</label>
+        <table id="process-feature-table">
+            <tr>
+                <th name="metric">Metric</th>
+                <th name="average">Average</th>
+                <th name="standard-deviation">Std. Dev.</th>
+                <th name="sum">Sum</th>
+                <th name="min">Min</th>
+                <th name="max">Max</th>
+                <th name="target-avg">Target Average</th>
+                <th name="target-sum">Target Sum</th>
+                <th name="ampel">Check</th>
+                <th name="info">Info</th>
+            </tr>`;
+
+        let feature_fulfillment = false;
+        let innerHTML_metrics = '';
+        Object.keys(metrics).forEach(function (key) {
+            let metric = metrics[key];
+            let metric_fulfillment, innerHTML_metric_row = fillMetricRows(metric, key, processData);
+        });
+
+        innerHTML += `</table>`;
+        innerHTML += '</div>';
+        innerHTML += '</nav>';
+        innerHTML += '</div>';
+        div.innerHTML = innerHTML;
+
+        // Append element to document
+        document.getElementById('metrics-input-processes').appendChild(div);
+    });
+}
+
+function fillMetricRows(metricData, slug, processData) {
+
+    // default value, because true has no influence on feature_fulfillment if metric_fulfillment is not given
+    let metric_fulfillment = true;
+
+    // default table row, when no metric data is provided
+    let innerHTML_actual = `
+                    <tr>
+                        <td id="${metricData['name']}">${metricData['name']}</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>`;
+    let innerHTML_target = `
+                        <td><input name="target-average" id="${slug}" value=""></td>`;
+    let innerHTML_fulfillment = `
+                        <td></td>
+                        <td></td>
+                        <td><img src="images/info.png" loading="lazy" width="35" alt="" class="info-icon"></td>
+                    </tr>`;
+
+    if(uid != null && uid != -1 && (slug in processData['actual_target_metrics'])) {
+
+        // check if actual values are provided
+        if('actual' in processData['actual_target_metrics'][slug]) {
+            innerHTML_actual = `
+                    <tr>
+                        <td id="${metricData['name']}">${metricData['name']}</td>
+                        <td>${processData['actual_target_metrics'][slug]['actual']['average']}</td>
+                        <td>${processData['actual_target_metrics'][slug]['actual']['standard_deviation']}</td>
+                        <td>${processData['actual_target_metrics'][slug]['actual']['total']}</td>
+                        <td>${processData['actual_target_metrics'][slug]['actual']['min']}</td>
+                        <td>${processData['actual_target_metrics'][slug]['actual']['max']}</td>`;
+        }
+
+        // check if a target value is provided
+        if('target' in processData['actual_target_metrics'][slug]) {
+            innerHTML_target =`
+                        <td><input name="target-average" id="${slug}" value="${processData['actual_target_metrics'][slug]['target']['average']}"></td>`
+        }
+
+        // check if a fulfillment and consequentially a target sum is provided (if fulfillment was calculated, a target sum was also able to be calculated)
+        if('fulfillment' in processData['actual_target_metrics'][slug]) {
+            let metric_fulfillment = processData['actual_target_metrics'][slug]['fulfillment'];
+            innerHTML_fulfillment = `
+                        <td>${processData['actual_target_metrics'][slug]['target']['total']}</td>
+                        <td>${renderCircle(metric_fulfillment)}</td>
+                        <td><img src="images/info.png" loading="lazy" width="35" alt="" class="info-icon"></td>
+                    </tr>`;
+        }
+    }
+
+    let innerHTML_metric_row = innerHTML_actual + innerHTML_target + innerHTML_fulfillment;
+
+    return metric_fulfillment, innerHTML_metric_row;
+}
+
+/**
+ * Render process ball for whole process.
+ * @param wholeProcessScore
+ */
+function renderWholeProcessScoreCircle(wholeProcessScore) {
+    let color;
+    wholeProcessScore = parseInt(wholeProcessScore);
+
+    if(wholeProcessScore < 80) {
+        color = "red";
+    } else if (wholeProcessScore < 90) {
+        color = "yellow"
+    } else {
+        color = "green";
+    }
+    document.getElementById("whole-process-score").setAttribute("style", `background-color:  ${color}`);
+    document.getElementById("whole-process-score").innerHTML = `${wholeProcessScore}%`;
+}
+
+/**
+ * Render ball for each metric.
+ *
+ * @param fulfillment
+ * @returns {string}
+ */
+function renderCircle(fulfillment) {
+    let color;
+    if(fulfillment) {
+        color = "green";
+    } else {
+        color = "red";
+    }
+
+    return `<div class="small-circle" style="background-color: ${color}"></div>`;
+}
+
 
 /**
  * This functions toggles the accordion
@@ -143,6 +302,7 @@ function toggleSection(element) {
         metric_child.style.position = "static";
     }
 }
+
 
 /**
  * This function saves the data entered to the database by transmitting the data to the backend
@@ -172,14 +332,14 @@ function createEditProcess() {
     if (typeof uid == undefined || uid == "" || uid == null) {
         uid = -1;
     }
-        const process = `{
+    const process = `{
         "process": {
             "uid": "${uid}",  
             "name": "${document.getElementById('process-name-textarea').value}",
             "description": "${document.getElementById('process-beschreibung-textarea').value}"
-    },
-        "target_metrics": ${JSON.stringify(metrics)}
-}`;
+        },
+            "target_metrics": ${JSON.stringify(metrics)}
+        }`;
 
     // Check if all field have been filled
     // Also, when changing between categories, discard inputs made for non-relevant metrics
@@ -191,11 +351,11 @@ function createEditProcess() {
         const input = toggles[i].value;
 
         // Check if enabled fields have been filled - all fields are required
-                if (toggles[i].value === '') {
-                    console.log(toggles[i].id);
-                    required_helper_flag = false;
-                }
-            }
+        if (toggles[i].value === '') {
+            console.log(toggles[i].id);
+            required_helper_flag = false;
+        }
+    }
 
     // If a input has been performed, post changes to backend
     if (required_helper_flag) {
@@ -210,6 +370,7 @@ function createEditProcess() {
     }
 }
 
+
 /**
  * Saves data.
  * @param data
@@ -220,137 +381,19 @@ function saveProcess(data) {
 
 
 /**
- * Render metrics section.
- *
- * @param {json} features
- * @param {json} processData
- */
-function createMetricsSection(features, processData) {
-    // Check if the request has succeeded
-    if (processData['success']) {
-        // Component data has been received
-
-        this.renderWholeProcessScoreCircle(processData['score']);
-
-        // Set uid and data fields
-        document.getElementById('process-name-textarea').value = processData['process']['name'];
-        document.getElementById('process-beschreibung-textarea').value = processData['process']['description'];
-    }
-
-        let featureCount=0;
-    Object.keys(features).forEach(function (key) {
-        featureCount++;
-        let feature = features[key];
-        let metrics = feature['metrics'];
-
-        let div = document.createElement('div');
-        div.id = key;
-        div.className = 'feature-section';
-
-        let innerHTML = '';
-        innerHTML += '<div data-hover="" data-delay="0" class="accordion-item">';
-        innerHTML += '<div class="accordion-toggle" onclick="toggleSection(this)">';
-        innerHTML += '<div class="accordion-icon"></div>';
-        innerHTML += ('<div class="features-label">' + feature['name'] + '</div>');
-        innerHTML += '</div>';
-        innerHTML += '<nav class="dropdown-list">';
-        innerHTML += '<div class="features-columns">';
-
-        // Table Headers
-        innerHTML += `
-        <label>Feature ${featureCount}: ${feature['name']}</label>
-        <table id="process-feature-table">
-            <tr>
-                <th name="metric">Metric</th>
-                <th name="average">Average</th>
-                <th name="standard-deviation">Std. Dev.</th>
-                <th name="sum">Sum</th>
-                <th name="min">Min</th>
-                <th name="max">Max</th>
-                <th name="target-avg">Target Average</th>
-                <th name="target-sum">Target Sum</th>
-                <th name="ampel">Check</th>
-                <th name="info">Info</th>
-            </tr>`;
-        Object.keys(metrics).forEach(function (key) {
-            let metric = metrics[key];
-
-            // default table row, when no metric data is provided
-            let innerHTML_actual = `
-                    <tr>
-                        <td id="${metric['name']}">${metric['name']}</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>`;
-            let innerHTML_target = `
-                        <td><input name="target-average" id="${key}" value=""></td>`;
-            let innerHTML_fulfillment = `
-                        <td></td>
-                        <td></td>
-                        <td><img src="images/info.png" loading="lazy" width="35" alt="" class="info-icon"></td>
-                    </tr>`;
-
-            if(uid != null && uid != -1 && processData['actual_target_metrics'][key]) {
-
-                // check if actual values are provided
-                if(processData['actual_target_metrics'][key]['actual']) {
-                    innerHTML_actual = `
-                    <tr>
-                        <td id="${metric['name']}">${metric['name']}</td>
-                        <td>${processData['actual_target_metrics'][key]['actual']['average']}</td>
-                        <td>${processData['actual_target_metrics'][key]['actual']['standard_deviation']}</td>
-                        <td>${processData['actual_target_metrics'][key]['actual']['total']}</td>
-                        <td>${processData['actual_target_metrics'][key]['actual']['min']}</td>
-                        <td>${processData['actual_target_metrics'][key]['actual']['max']}</td>`;
-                }
-
-                // check if a target value is provided
-                if(processData['actual_target_metrics'][key]['target']) {
-                    innerHTML_target =`
-                        <td><input name="target-average" id="${key}" value="${processData['actual_target_metrics'][key]['target']['average']}"></td>`
-                }
-
-                // check if a fulfillment and consequentially a target sum is provided (if fulfillment was calculated, a target sum was also able to be calculated)
-                if(processData['actual_target_metrics'][key]['fulfillemt']) {
-                    innerHTML_fulfillment = `
-                        <td>${processData['actual_target_metrics'][key]['target']['total']}</td>
-                        <td>${renderCircle(processData['actual_target_metrics'][key]['fulfillment'])}</td>
-                        <td><img src="images/info.png" loading="lazy" width="35" alt="" class="info-icon"></td>
-                    </tr>`;
-                }
-            }
-
-            innerHTML += innerHTML_actual + innerHTML_target + innerHTML_fulfillment;
-
-        });
-
-    innerHTML += `</table>`;
-    innerHTML += '</div>';
-        innerHTML += '</nav>';
-        innerHTML += '</div>';
-        div.innerHTML = innerHTML;
-
-        // Append element to document
-        document.getElementById('metrics-input-processes').appendChild(div);
-    });
-}
-
-/**
  * This function checks for success in communication
  *
  * @param {string} response: JSON Object response, whether the changes have been saved successfully
  */
 
 function saveCallback(response) {
-    // Check if component has been created/edited successfully
+    // Check if process has been created/edited successfully
     if (response['success']) {
         // Component has been created/edited successfully
         window.alert('Changes were saved.');
         window.location = base_url;
     } else {
-        // Component has not been created/edited successfully
+        // Process has not been created/edited successfully
         window.alert('Changes could not be saved.');
     }
 }
