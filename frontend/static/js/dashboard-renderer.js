82a244c01e52583e6410b0c2bcc000dc1b496d0f
Jan-Tiedemann <43883681+Jan-Tiedemann@users.noreply.github.com>
2021-04-14 11:21:59 +0200

    285_Modal reminder eingebaut

--- frontend/static/js/dashboard-renderer.js
+++ frontend/static/js/dashboard-renderer.js
@@ -221,3 +221,33 @@ function deleteCallback(response) {
         window.alert('Object could not be deleted.');
     }
 }
+
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+var modalbtn = document.getElementById("modalbtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Open modal every 2 hours
+setInterval(function(){
+    modal.style.display = "block";},7200000);
+
+// When the user clicks on the test button, open the modal
+modalbtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
