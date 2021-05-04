6d35d9da04f0b9c312fef49308b44e92cfb3289f
N3R1K <erik.neumann999@web.de>
2021-05-04 18:29:18 +0200

    applied new naming convention + new unittest

--- unittesting/test_calculations.py
+++ unittesting/test_calculations.py
@@ -2,79 +2,9 @@ import unittest
 import processing.calculations as calc
 import database.handler.metric_handler as metric_handler
 
+##multiple used dicts (for various functions)
 
-process_dict = {
-    "success": True,
-    "process": {
-        "uid": "b141f94973a43cf8ee972e9dffc1b004",
-        "name": "Kunde anlegen",
-        "description": "Prozess zum anlegen von einem neuen Kunden in allen Systemen",
-        "creation_timestamp": "20210210...",
-        "last_timestamp": "20200211...",
-        "components": [
-            {
-                "uid": "b141f94973a43cf8ee972e9dffc1b004",
-                "weight": 1,  # different from single component view!
-                "name": "SQL Datenbank",
-                "category": "Datenbank",
-                "description": "Kundendatenbank",
-                "creation_timestamp": "20200219...",
-                "last_timestamp": "20200219...",
-                "metrics": {
-                    "codelines": 20000,
-                    "admins": 10,
-                    "recovery_time": 5,
-                    # ...
-                }
-            },
-            {
-                "uid": "b141f94973a43cf8ee972e9dffc1b004",
-                "weight": 1.5,
-                "name": "Frontend API",
-                "category": "API",
-                "description": "API für das Frontend",
-                "creation_timestamp": "20200219...",
-                "last_timestamp": "20200219...",
-                "metrics": {
-                    "codelines": 20000,
-                    "admins": 10,
-                    "recovery_time": 5,
-                    # ...
-                }
-            },
-            {
-                "uid": "b141f94973a43cf8ee972e9dffc1b004",
-                "weight": 2,
-                "name": "Hadoop Cluster",
-                "category": "Datenbank",
-                "description": "Big Data Plattform",
-                "creation_timestamp": "20200219...",
-                "last_timestamp": "20200219...",
-                "metrics": {
-                    "codelines": 20000,
-                    "admins": 10,
-                    "recovery_time": 5,
-                    # ...
-                }
-            }
-            # ...
-        ]
-    },
-    "target_metrics": {
-        "codelines": {
-            "average": 50,
-            "min": 30.5,
-            "max": 20,
-        },
-        "admins": {
-            "average": 50,
-            "min": 30.5,
-            "max": 20,
-        },
-        # ...
-    },
-}
-
+#process_dicts with different valid process_dicts
 process_dict1 = {
     "success": True,
     "process": {
@@ -93,8 +23,8 @@ process_dict1 = {
                 "creation_timestamp": "20200219...",
                 "last_timestamp": "20200219...",
                 "metrics": {
-                    "admins": 10,
                     "codelines": 20000,
+                    "admins": 10,
                     "recovery_time": 5,
                     # ...
                 }
@@ -164,6 +94,221 @@ process_dict2 = {
                 "creation_timestamp": "20200219...",
                 "last_timestamp": "20200219...",
                 "metrics": {
+                    "codelines": 40000,
+                    "admins": 20,
+                    "recovery_time": 10,
+                    # ...
+                }
+            },
+            {
+                "uid": "b141f94973a43cf8ee972e9dffc1b004",
+                "weight": 1.5,
+                "name": "Frontend API",
+                "category": "API",
+                "description": "API für das Frontend",
+                "creation_timestamp": "20200219...",
+                "last_timestamp": "20200219...",
+                "metrics": {
+                    "codelines": 10000,
+                    "admins": 5,
+                    "recovery_time": 3,
+                    # ...
+                }
+            },
+            {
+                "uid": "b141f94973a43cf8ee972e9dffc1b004",
+                "weight": 2,
+                "name": "Hadoop Cluster",
+                "category": "Datenbank",
+                "description": "Big Data Plattform",
+                "creation_timestamp": "20200219...",
+                "last_timestamp": "20200219...",
+                "metrics": {
+                    "codelines": 50000,
+                    "admins": 70,
+                    "recovery_time": 50,
+                    # ...
+                }
+            }
+            # ...
+        ]
+    },
+    "target_metrics": {
+        "codelines": {
+            "average": 50,
+            "min": 30.5,
+            "max": 20,
+        },
+        "admins": {
+            "average": 50,
+            "min": 30.5,
+            "max": 20,
+        },
+        # ...
+    },
+}
+process_dict3 = {
+    "success": True,
+    "process": {
+        "uid": "b141f94973a43cf8ee972e9dffc1b004",
+        "name": "Kunde anlegen",
+        "description": "Prozess zum anlegen von einem neuen Kunden in allen Systemen",
+        "creation_timestamp": "20210210...",
+        "last_timestamp": "20200211...",
+        "components": [
+            {
+                "uid": "b141f94973a43cf8ee972e9dffc1b004",
+                "weight": 1,  # different from single component view!
+                "name": "SQL Datenbank",
+                "category": "Datenbank",
+                "description": "Kundendatenbank",
+                "creation_timestamp": "20200219...",
+                "last_timestamp": "20200219...",
+                "metrics": {
+                    "codelines": 50000,
+                    "admins": 70,
+                    "recovery_time": 4,
+                    # ...
+                }
+            },
+            {
+                "uid": "b141f94973a43cf8ee972e9dffc1b004",
+                "weight": 1.5,
+                "name": "Frontend API",
+                "category": "API",
+                "description": "API für das Frontend",
+                "creation_timestamp": "20200219...",
+                "last_timestamp": "20200219...",
+                "metrics": {
+                    "codelines": 40000,
+                    "admins": 50,
+                    "recovery_time": 6,
+                    # ...
+                }
+            },
+            {
+                "uid": "b141f94973a43cf8ee972e9dffc1b004",
+                "weight": 2,
+                "name": "Hadoop Cluster",
+                "category": "Datenbank",
+                "description": "Big Data Plattform",
+                "creation_timestamp": "20200219...",
+                "last_timestamp": "20200219...",
+                "metrics": {
+                    "codelines": 50000,
+                    "admins": 60,
+                    "recovery_time": 4,
+                    # ...
+                }
+            }
+            # ...
+        ]
+    },
+    "target_metrics": {
+        "codelines": {
+            "average": 50,
+            "min": 30.5,
+            "max": 20,
+        },
+        "admins": {
+            "average": 50,
+            "min": 30.5,
+            "max": 20,
+        },
+        # ...
+    },
+}
+
+#process dicts with order changes of metrics, but identically values
+test1_process_dict = {
+    "success": True,
+    "process": {
+        "uid": "b141f94973a43cf8ee972e9dffc1b004",
+        "name": "Kunde anlegen",
+        "description": "Prozess zum anlegen von einem neuen Kunden in allen Systemen",
+        "creation_timestamp": "20210210...",
+        "last_timestamp": "20200211...",
+        "components": [
+            {
+                "uid": "b141f94973a43cf8ee972e9dffc1b004",
+                "weight": 1,  # different from single component view!
+                "name": "SQL Datenbank",
+                "category": "Datenbank",
+                "description": "Kundendatenbank",
+                "creation_timestamp": "20200219...",
+                "last_timestamp": "20200219...",
+                "metrics": {
+                    "admins": 10,
+                    "codelines": 20000,
+                    "recovery_time": 5,
+                    # ...
+                }
+            },
+            {
+                "uid": "b141f94973a43cf8ee972e9dffc1b004",
+                "weight": 1.5,
+                "name": "Frontend API",
+                "category": "API",
+                "description": "API für das Frontend",
+                "creation_timestamp": "20200219...",
+                "last_timestamp": "20200219...",
+                "metrics": {
+                    "codelines": 20000,
+                    "admins": 10,
+                    "recovery_time": 5,
+                    # ...
+                }
+            },
+            {
+                "uid": "b141f94973a43cf8ee972e9dffc1b004",
+                "weight": 2,
+                "name": "Hadoop Cluster",
+                "category": "Datenbank",
+                "description": "Big Data Plattform",
+                "creation_timestamp": "20200219...",
+                "last_timestamp": "20200219...",
+                "metrics": {
+                    "codelines": 20000,
+                    "admins": 10,
+                    "recovery_time": 5,
+                    # ...
+                }
+            }
+            # ...
+        ]
+    },
+    "target_metrics": {
+        "codelines": {
+            "average": 50,
+            "min": 30.5,
+            "max": 20,
+        },
+        "admins": {
+            "average": 50,
+            "min": 30.5,
+            "max": 20,
+        },
+        # ...
+    },
+}
+test2_process_dict = {
+    "success": True,
+    "process": {
+        "uid": "b141f94973a43cf8ee972e9dffc1b004",
+        "name": "Kunde anlegen",
+        "description": "Prozess zum anlegen von einem neuen Kunden in allen Systemen",
+        "creation_timestamp": "20210210...",
+        "last_timestamp": "20200211...",
+        "components": [
+            {
+                "uid": "b141f94973a43cf8ee972e9dffc1b004",
+                "weight": 1,  # different from single component view!
+                "name": "SQL Datenbank",
+                "category": "Datenbank",
+                "description": "Kundendatenbank",
+                "creation_timestamp": "20200219...",
+                "last_timestamp": "20200219...",
+                "metrics": {
                     "codelines": 20000,
                     "recovery_time": 5,
                     "admins": 10,
@@ -217,7 +362,7 @@ process_dict2 = {
         # ...
     },
 }
-process_dict3 = {
+test3_process_dict = {
     "success": True,
     "process": {
         "uid": "b141f94973a43cf8ee972e9dffc1b004",
@@ -289,34 +434,50 @@ process_dict3 = {
     },
 }
 
-
-#metrics_dict = metric_handler.get_metrics_data()
-#print(metrics_dict)
-
-#4200
+"""
+metrics_dict = metric_handler.get_metrics_data()
+print(metrics_dict)
 component_metrics = calc.get_all_component_metrics(process_dict)
-#print(component_metrics)
-test1_component_metrics = {'codelines': [20000, 20000, 20000], 'admins': [10, 10, 10], 'recovery_time': [5, 5, 5]}
+print(component_metrics)
+print(process_dict)
+print(component_metrics)
+print(test1_component_metrics)
+"""
 
-#4300
-#target_metrics = calc.get_all_target_metrics(process_dict)
-#print(target_metrics)
+#42xx - get_all_component_metrics
+
+test1_component_metrics = calc.get_all_component_metrics(process_dict1)
+test2_component_metrics = calc.get_all_component_metrics(process_dict2)
+test3_component_metrics = calc.get_all_component_metrics(process_dict3)
 
 
 class TestCalc(unittest.TestCase):
 
-    def test_compare_actual_target_metrics(self):
+    def test_get_all_component_metrics_4201(self):
+        print('4201 - Überprüfen, ob alle Metriken ausgelesen werden')
         self.assertEqual(calc.get_all_component_metrics(process_dict1),
                          test1_component_metrics),
         self.assertEqual(calc.get_all_component_metrics(process_dict2),
-                         test1_component_metrics),
+                         test2_component_metrics),
         self.assertEqual(calc.get_all_component_metrics(process_dict3),
+                         test3_component_metrics)
+
+    def test_get_all_component_metrics_4203(self):
+        print('4203 - Überprüfen, ob die Reihenfolge der process_dict Elemente variieren kann')
+        self.assertEqual(calc.get_all_component_metrics(test1_process_dict),
                          test1_component_metrics),
-#print(process_dict)
-#print(component_metrics)
-#print(test1_component_metrics)
+        self.assertEqual(calc.get_all_component_metrics(test2_process_dict),
+                         test1_component_metrics),
+        self.assertEqual(calc.get_all_component_metrics(test3_process_dict),
+                         test1_component_metrics)
+
+
+
+#4300
+
+#target_metrics = calc.get_all_target_metrics(process_dict1)
+#print(target_metrics)
 
 
 if __name__ == '__main__':
     unittest.main()
-
