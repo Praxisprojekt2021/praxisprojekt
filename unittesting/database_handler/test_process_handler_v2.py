dd30251300008d23bc303292b2eda4247368c1bc
Timo Buecher <mail@timo-buecher.de>
2021-05-10 19:16:57 +0200

    262: tests for get_process

---
+++ unittesting/database_handler/test_process_handler_v2.py
@@ -0,0 +1,52 @@
+import unittest
+
+from unittesting.database_handler.test_data_processs_handler import *
+from database.handler.process_handler import *
+
+
+class TestGetComponent(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.maxDiff = None
+        # get old process_list
+        process_list_pre = get_process_list()['processes']
+        # add new component
+        add_process(GET_PROCESS_SETUP_AND_OUT)
+        # get new process_list
+        process_list_post = get_process_list()['processes']
+
+        cls.uid = None
+
+        for post_uid in process_list_post:
+            if post_uid not in process_list_pre:
+                cls.uid = post_uid['uid']
+
+        GET_PROCESS_IN["uid"] = cls.uid
+        GET_PROCESS_SETUP_AND_OUT["process"]["uid"] = cls.uid
+
+    def test_2001(self):
+        is_dict = get_process(GET_PROCESS_IN)
+        is_dict["process"].pop("creation_timestamp")
+        is_dict["process"].pop("last_timestamp")
+        self.assertEqual(is_dict, GET_PROCESS_SETUP_AND_OUT)
+
+    def test_2002_2003(self):
+        uids_to_be_tested = [1.5, 'abc']
+
+        for uid in uids_to_be_tested:
+            GET_PROCESS_IN['uid'] = uid
+            with self.assertRaises(IndexError):
+                get_process(GET_PROCESS_IN)
+
+    def test_2004(self):
+
+        GET_PROCESS_IN.pop("uid")
+        GET_PROCESS_IN["test1"] = 12
+        with self.assertRaises(KeyError):
+            get_process(GET_PROCESS_IN)
+
+    @classmethod
+    def tearDownClass(cls):
+        GET_PROCESS_IN["uid"] = cls.uid
+        print(GET_PROCESS_IN)
+        delete_process(GET_PROCESS_IN)
