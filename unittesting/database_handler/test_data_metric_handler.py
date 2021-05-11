48a60c5c34f0c09ce3e2c6ebbfc07a803730f4a8
zorvin <corvin.fassbender@web.de>
2021-05-11 14:30:37 +0200

    262_UnitTesting: Modulized the dicts for the metric handler. Added an Unit Test for getMetrics() and implemented setUpClass and tearDownClass where needed.

---
+++ unittesting/database_handler/test_data_metric_handler.py
@@ -0,0 +1,13 @@
+CREATE_FROM_FRONTEND_JSON = {
+    'success': True,
+    'metrics': {
+        'automation': {
+            'uid': 'd0cfbd93a0d146d9a223b7cfad098a7a',
+            'fulfilled_if': '>'
+        },
+        'test_automation': {
+            'uid': '1de621af9cb7404291a55671463b5ad7',
+            'fulfilled_if': '>'
+        },
+    }
+}
