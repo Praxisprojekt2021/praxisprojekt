f639d8fe6dbb990f32bca83072cf5e7cc8d25b06
Timo Buecher <mail@timo-buecher.de>
2021-05-11 11:10:55 +0200

    262: process delete reference tests

--- unittesting/database_handler/test_process_handler_v2.py
+++ unittesting/database_handler/test_process_handler_v2.py
@@ -145,13 +145,11 @@ class TestUpdateProcess(unittest.TestCase):
     def test_2204(self):
         process_dict = get_process(GET_PROCESS_IN)
         process_dict["process"]["uid"] = "ABC"
-        print(update_process(process_dict))
 
         # TODO: Is this the right Exception?
-        """
         with self.assertRaises(CypherSyntaxError):
             update_process(process_dict)
-        """
+
 
     @classmethod
     def tearDownClass(cls):
@@ -265,6 +263,73 @@ class TestAddProcessReference(unittest.TestCase):
 
         with self.assertRaises(KeyError):
             add_process_reference(ADD_PROCESS_REFERENCE_IN)
+
+    @classmethod
+    def tearDownClass(cls):
+        GET_PROCESS_IN["uid"] = cls.uid
+        delete_process(GET_PROCESS_IN)
+        GET_PROCESS_IN["uid"] = cls.componentUid
+        delete_component(GET_PROCESS_IN)
+
+
+class TestDeleteProcessReference(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.maxDiff = None
+        # get old process_list
+        process_list_pre = get_process_list()['processes']
+        # add new component
+        add_process(GET_PROCESS_SETUP_AND_OUT)
+        # get new process_list
+        process_list_post = get_process_list()['processes']
+
+        cls.uid = None
+        cls.componentUid = None
+
+        for post_uid in process_list_post:
+            if post_uid not in process_list_pre:
+                cls.uid = post_uid['uid']
+
+        GET_PROCESS_IN["uid"] = cls.uid
+        GET_PROCESS_SETUP_AND_OUT["process"]["uid"] = cls.uid
+
+
+        component_list_pre = get_component_list()['components']
+        # add new component
+        add_component(ADD_COMPONENT_IN)
+        component_list_post = get_component_list()['components']
+
+        for post_uid in component_list_post:
+            if post_uid not in component_list_pre:
+                cls.componentUid = post_uid['uid']
+
+    def setUp(self):
+        ADD_PROCESS_REFERENCE_IN["process_uid"] = self.uid
+        add_process_reference(ADD_PROCESS_REFERENCE_IN)
+
+
+    def test_2501(self):
+        ADD_PROCESS_REFERENCE_IN["uid"] = self.uid
+        self.assertEqual(delete_process_reference(ADD_PROCESS_REFERENCE_IN), success_handler())
+
+    def test_2502(self):
+        ADD_PROCESS_REFERENCE_IN["uid"] = "ABC"
+
+        # TODO: Is this the right Exception?
+        with self.assertRaises(CypherSyntaxError):
+            delete_process_reference(ADD_PROCESS_REFERENCE_IN)
+            ADD_PROCESS_REFERENCE_IN["uid"] = self.uid
+            delete_process(ADD_PROCESS_REFERENCE_IN)
+
+    def test_2503(self):
+        ADD_PROCESS_REFERENCE_IN.pop("uid")
+        ADD_PROCESS_REFERENCE_IN["ABC"] = 12
+
+        with self.assertRaises(KeyError):
+            delete_process(ADD_PROCESS_REFERENCE_IN)
+            ADD_PROCESS_REFERENCE_IN["uid"] = self.uid
+            delete_process(ADD_PROCESS_REFERENCE_IN)
 
     @classmethod
     def tearDownClass(cls):
